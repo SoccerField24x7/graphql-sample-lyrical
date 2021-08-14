bb1895f32d523e88e613cc53eb5c1078c1ae55c1
Jesse Quijano <jesse@quijano.net>
2021-08-13 20:10:19 -0700

    add mutation, back button, refetch on successful add

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -8,14 +12,22 @@ class SongCreate extends Component {
     }
 
     onSubmit(event) {
-        event.preventDevault();
+        event.preventDefault();
 
-        
-    } 
+        this.props.mutate({
+            variables: { title: this.state.title },
+            refetchQueries: [{ query: query }]
+        }).then(() => {
+            hashHistory.push('/');
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 
     render() {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a New Song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
@@ -26,4 +38,12 @@ class SongCreate extends Component {
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title) {
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
