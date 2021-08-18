7f6a3177436831f1c058b1955914bf9c13550cb1
Jesse Quijano <jesse@quijano.net>
2021-08-18 16:26:14 -0700

    assign the data id so apollo can properly track objects

--- client/index.js
+++ client/index.js
@@ -10,7 +10,9 @@ import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
 import SongDetail from './components/SongDetail';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+    dataIdFromObject: o => o.id // only works if all id's are unique
+});
 
 const Root = () => {
   return (
